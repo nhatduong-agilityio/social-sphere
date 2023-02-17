9ab692d93f67b778f00fc0a90f03c8d67f3afe4e
nhatduongcong <nhat.duong@asnet.com.vn>
2023-02-17 13:49:48 +0700

    FEAT: Create folder and constants in project

---
+++ src/constant/action.tsx
@@ -0,0 +1,8 @@
+import { actionTypes } from '~/types/common';
+
+// Constant fetch users.
+export const FETCH_USERS: actionTypes = {
+  PENDING: 'ACTION_TYPES_PENDING',
+  SUCCESS: 'ACTION_TYPES_SUCCESS',
+  FAILURE: 'ACTION_TYPES_FAILURE',
+};
---
+++ src/constant/urls.tsx
@@ -0,0 +1,13 @@
+// Constants api with json-server
+const API = {
+  API_HOST: 'http://localhost:3000',
+  PATH_USERS: '/users',
+};
+
+// Constants api with json-server
+const URL = {
+  API_BASE_URL: `${API.API_HOST}`,
+  API_BASE_USERS: `${API.API_HOST}${API.PATH_USERS}`,
+};
+
+export { URL };
