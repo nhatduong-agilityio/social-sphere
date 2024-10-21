5f66e166c6322f65dc038f3be6c45b3f0e37f413
Loc Thanh Vo <loc.vo@asnet.com.vn>
2024-10-21 16:06:03 +0700

    feat: implement notification widget

--- frontend/src/features/home/components/activity-feed.tsx
+++ frontend/src/features/home/components/activity-feed.tsx
@@ -1,11 +1,23 @@
 'use client';
+import { Briefcase, Gift } from 'lucide-react';
 
 import { ComposeFeedCard } from './compose-feed-card';
+import NotificationWidget from './notification-widget';
 import StoriesWidget from './stories-widget';
 import SuggestFriendsWidget from './suggest-friends-widget';
 
 // Hooks
 import { useDisclosure } from '@/hooks/use-disclosure';
+
+// Mocks
+import { MOCK_FRIENDS } from '@/__mocks__/user';
+
+// Images
+import AvatarPlaceholder from '../../../../public/images/avatar-placeholder.svg';
+
+// Icons
+import { BirthdayIcon } from '@/icons/birthday-icon';
+import { JobIcon } from '@/icons/job-icon';
 
 export const ActivityFeed = () => {
   const {
@@ -33,7 +45,27 @@ export const ActivityFeed = () => {
         </div>
         <div className="hidden lg:flex col-span-3 flex-col gap-6">
           <StoriesWidget onAddStory={handleAddStory} />
+          <NotificationWidget
+            avatar={MOCK_FRIENDS[0].avatar || AvatarPlaceholder.src}
+            iconContent={<BirthdayIcon />}
+            customClass="bg-green-100 dark:bg-green-100"
+            title="Dan turns 31 today!"
+            description="Send him your best wishes by leaving something on his wall."
+            iconHeader={<Gift size={24} className="text-white" />}
+            styleNotificationClass="border-2 border-green-100"
+            notificationCount={27}
+          />
           <SuggestFriendsWidget />
+
+          <NotificationWidget
+            avatar={MOCK_FRIENDS[0].avatar || AvatarPlaceholder.src}
+            iconContent={<JobIcon />}
+            customClass="bg-blue-600 dark:bg-blue-600"
+            title="Nelly has a new job!"
+            description="Send her message congratulating her for getting this job."
+            iconHeader={<Briefcase size={24} className="text-white" />}
+            styleNotificationClass="border-2 border-blue-600"
+          />
         </div>
       </div>
     </div>
