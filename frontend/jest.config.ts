d55942c59c63a84e99519e3254d4c12a7ae03138
Nhat Cong Duong <nhat.duong@asnet.com.vn>
2024-10-23 15:40:00 +0700

    Feature / Update husky config

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -4,7 +4,7 @@ import nextJest from 'next/jest.js';
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
-})
+});
 
 // Add any custom config to be passed to Jest
 const config: Config = {
@@ -14,10 +14,15 @@ const config: Config = {
     '<rootDir>/src/**/*.{js,jsx,ts,tsx}',
     '!**/*.d.ts',
     '!<rootDir>/src/**/*.stories.{js,jsx,ts,tsx}',
-    '!<rootDir>/node_modules/'
+    '!<rootDir>/node_modules/',
   ],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-}
+  moduleNameMapper: {
+    'next-auth/providers/credentials':
+      '<rootDir>/src/__mocks__/next-auth-providers-credentials.ts',
+    'next-auth': '<rootDir>/src/__mocks__/next-auth.ts',
+  },
+};
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-export default createJestConfig(config)
+export default createJestConfig(config);
