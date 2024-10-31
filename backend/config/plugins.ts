4d209bd96cbab0cf970fbe3df421f27db30913c4
Nhat Cong Duong <nhat.duong@asnet.com.vn>
2024-10-31 16:36:43 +0700

    Feature / Implement strapi with cloudinary

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -12,4 +12,14 @@ export default () => ({
       },
     },
   },
+  upload: {
+    config: {
+      provider: '@strapi/provider-upload-cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+    },
+  },
 });
