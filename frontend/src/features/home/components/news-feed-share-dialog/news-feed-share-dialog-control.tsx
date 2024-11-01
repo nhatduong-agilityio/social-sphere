8c40a89642d0ba23b45b39b4f5d302a4cb1eb86f
Nhat Cong Duong <nhat.duong@asnet.com.vn>
2024-11-01 14:16:41 +0700

    Feature / Implement share actions on control

--- frontend/src/features/home/components/news-feed-share-dialog/news-feed-share-dialog-control.tsx
+++ frontend/src/features/home/components/news-feed-share-dialog/news-feed-share-dialog-control.tsx
@@ -1,48 +1,88 @@
 'use client';
 
-import { memo } from 'react';
+import { memo, useMemo } from 'react';
+import { UseFormReturn } from 'react-hook-form';
 
 // Icons
 import { AccountPlusIcon, LockClockIcon, MapMarkerIcon } from '@/icons';
 
 // Components
 import { Button, DialogClose, DialogFooter } from '@/components/ui';
+import { ShareActivityDropdown } from './share-activity-dropdown';
 
-const CONTROL_BUTTONS = [
-  { icon: AccountPlusIcon },
-  { icon: MapMarkerIcon },
-  { icon: LockClockIcon },
-];
+// Hooks
+import { ShareFormValues } from '../../hooks';
 
-export const NewsFeedShareDialogControl = memo(() => (
-  <DialogFooter className="flex w-full sm:justify-between p-2 bg-black-haze-50 dark:bg-transparent border-t border-gray-600 dark:border-dark-500 rounded-b-xl">
-    <div className="flex">
-      {CONTROL_BUTTONS.map(({ icon: Icon }, index) => (
-        <Button
-          key={index}
-          type="button"
-          variant="unstyle"
-          className="w-[34px] h-[34px] p-0 rounded-[4px] text-neutral-200 hover:dark:text-white hover:bg-gray-600 hover:dark:bg-dark-400"
-        >
-          <Icon className="w-[18px] h-[18px]" />
-        </Button>
-      ))}
-    </div>
-    <div className="flex gap-2">
-      <DialogClose asChild>
-        <Button
-          type="button"
-          variant="fixed"
-          className="h-8 text-2xs px-5 border-gray-600 bg-gray-600 hover:bg-gray-800"
-        >
-          Cancel
-        </Button>
-      </DialogClose>
-      <Button variant="primary" className="h-8 text-2xs px-5 bg-blue-600">
-        Publish
-      </Button>
-    </div>
-  </DialogFooter>
-));
+interface NewsFeedShareDialogControlProps {
+  form: UseFormReturn<ShareFormValues>;
+  onOpenTagFriends: () => void;
+  onOpenLocation: () => void;
+}
+
+export const NewsFeedShareDialogControl = memo(
+  ({
+    form,
+    onOpenTagFriends,
+    onOpenLocation,
+  }: NewsFeedShareDialogControlProps) => {
+    const CONTROL_BUTTONS = useMemo(
+      () => [
+        {
+          icon: AccountPlusIcon,
+          onClick: onOpenTagFriends,
+        },
+        {
+          icon: MapMarkerIcon,
+          onClick: onOpenLocation,
+        },
+      ],
+      [onOpenLocation, onOpenTagFriends],
+    );
+
+    return (
+      <DialogFooter className="flex w-full sm:justify-between p-2 bg-black-haze-50 dark:bg-transparent border-t border-gray-600 dark:border-dark-500 rounded-b-xl">
+        <div className="flex">
+          {CONTROL_BUTTONS.map(({ icon: Icon, onClick }, index) => (
+            <Button
+              key={index}
+              type="button"
+              variant="unstyle"
+              className="w-[34px] h-[34px] p-0 rounded-[4px] text-neutral-200 hover:dark:text-white hover:bg-gray-600 hover:dark:bg-dark-400"
+              onClick={onClick}
+            >
+              <Icon className="w-[18px] h-[18px]" />
+            </Button>
+          ))}
+          <ShareActivityDropdown
+            trigger={
+              <Button
+                type="button"
+                variant="unstyle"
+                className="w-[34px] h-[34px] p-0 rounded-[4px] text-neutral-200 hover:dark:text-white hover:bg-gray-600 hover:dark:bg-dark-400"
+              >
+                <LockClockIcon className="w-[18px] h-[18px]" />
+              </Button>
+            }
+            form={form}
+          />
+        </div>
+        <div className="flex gap-2">
+          <DialogClose asChild>
+            <Button
+              type="button"
+              variant="fixed"
+              className="h-8 text-2xs px-5 border-gray-600 bg-gray-600 hover:bg-gray-800"
+            >
+              Cancel
+            </Button>
+          </DialogClose>
+          <Button variant="primary" className="h-8 text-2xs px-5 bg-blue-600">
+            Publish
+          </Button>
+        </div>
+      </DialogFooter>
+    );
+  },
+);
 
 NewsFeedShareDialogControl.displayName = 'NewsFeedShareDialogControl';
---
+++ frontend/src/features/home/components/news-feed-share-dialog/share-activity-dropdown.tsx
@@ -0,0 +1,105 @@
+'use client';
+
+import { Fragment, memo, ReactNode, useMemo } from 'react';
+import { UseFormReturn } from 'react-hook-form';
+
+// Constants
+import { ACTIVITY_ROLES } from '../../constants';
+
+// Components
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+  Heading,
+  Text,
+  Label,
+} from '@/components/ui';
+
+// Hooks
+import { ShareFormValues } from '../../hooks';
+
+interface ShareActivityDropdownProps {
+  trigger: ReactNode;
+  form: UseFormReturn<ShareFormValues>;
+}
+
+export const ShareActivityDropdown = memo(
+  ({ trigger, form }: ShareActivityDropdownProps) => {
+    const value = form.watch('activityRole');
+
+    const selectedOption = useMemo(
+      () =>
+        ACTIVITY_ROLES.find((role) => role.value === value) ||
+        ACTIVITY_ROLES[0],
+      [value],
+    );
+
+    return (
+      <FormField
+        control={form.control}
+        name="activityRole"
+        render={({ field: optionField }) => (
+          <FormItem>
+            <FormControl>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <div className="flex items-center">
+                    {trigger}
+                    {selectedOption !== ACTIVITY_ROLES[0] && (
+                      <Label
+                        variant="darkNeutral"
+                        className="text-2xs user-select-none font-sans"
+                      >
+                        {selectedOption.label}
+                      </Label>
+                    )}
+                  </div>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-[280px] p-0 py-2">
+                  {ACTIVITY_ROLES.map(
+                    ({ label, description, value, icon: Icon }, index) => (
+                      <Fragment key={label}>
+                        {index === ACTIVITY_ROLES.length - 1 && (
+                          <DropdownMenuSeparator />
+                        )}
+                        <DropdownMenuItem
+                          key={label}
+                          onClick={() => optionField.onChange(value)}
+                          className="flex items-center gap-2.5 py-2 px-4 h-[50px]"
+                        >
+                          {Icon}
+                          <div className="flex flex-col">
+                            <Heading
+                              headingLevel="h3"
+                              className="text-xs font-roboto"
+                            >
+                              {label}
+                            </Heading>
+                            <Text
+                              variant="primary"
+                              className="text-2xs font-normal"
+                            >
+                              {description}
+                            </Text>
+                          </div>
+                        </DropdownMenuItem>
+                      </Fragment>
+                    ),
+                  )}
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </FormControl>
+          </FormItem>
+        )}
+      />
+    );
+  },
+);
+
+ShareActivityDropdown.displayName = 'ShareActivityDropdown';
